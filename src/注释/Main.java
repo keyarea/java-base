a2b41452d765d8b690432ff9e769be31db3beca6
Key <mail@loong.io>
2019-01-15 17:04:59 +0800

    first commit

---
+++ src/注释/Main.java
@@ -0,0 +1,18 @@
+package 注释;
+
+
+/**
+ * 这是文档注释,可以用来自动的生成文档
+ * @version 0.0.1 2019-01-15
+ * @author keyarea
+ */
+public class Main {
+    public static void main(String[] args){
+        // 单行注释,其注释内容从开始到本行结尾
+
+        /*
+        多行注释
+         */
+
+    }
+}
---
+++ src/第一个java程序/Main.java
@@ -0,0 +1,17 @@
+package 第一个java程序;
+
+// 第一个java程序
+
+// 主程序所在的类称之为主类，而主类一般使用public class进行声明。
+public class Main {
+    // 主方法是一切的起点，也就是说所有的程序都是通过主方法开始执行的。
+    // 主方法编写的代码才是你整个程序该执行的程序逻辑。
+    public static void main(String[] args) {
+        // 输出字符串
+        // 指的是在 屏幕上进行信息的打印操作，而信息输出有以下两类语法：
+        //
+        //输出后换行：System.out.println(内容)；
+        //输出后不换行：System.out.print(内容)；
+        System.out.println("Hello World!");
+    }
+}
